a2ca8fe6d3f2a3bb31df0cff01964b4f6d183ab9
Punam-Dev <punam.pal.dev@gmail.com>
2023-06-18 18:33:47 +0530

    Added validation

--- PhoneBookTestApp/PhoneBookTestApp/PhoneBook.cs
+++ PhoneBookTestApp/PhoneBookTestApp/PhoneBook.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Data;
 using System.Data.SQLite;
 using System.Threading.Tasks;
@@ -15,19 +16,52 @@ namespace PhoneBookTestApp
 
         public async Task AddPerson(Person person)
         {
+            if (person is null)
+            {
+                throw new ArgumentNullException(nameof(person));
+            }
+
+            if (string.IsNullOrEmpty(person.name))
+            {
+                throw new ArgumentNullException(nameof(person.name));
+            }
+
             SQLiteParameter[] parameters = new SQLiteParameter[]
+            {
+                new SQLiteParameter("@name", $"%{person.name}%"),
+            };
+            string commandText = "SELECT * FROM PHONEBOOK WHERE NAME LIKE @name LIMIT 1";
+
+            var existingPerson = await _repository.GetRecordAsync(commandText, CommandType.Text, parameters);
+
+            if(existingPerson != null)
+            {
+                throw new ArgumentException($"Person with name {person.name} already exists in db");
+            }
+
+            parameters = new SQLiteParameter[]
             {
                 new SQLiteParameter("@name", person.name),
                 new SQLiteParameter("@phoneNumber", person.phoneNumber),
                 new SQLiteParameter("@address", person.address),
             };
-            string commandText = "INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES(@name, @phoneNumber, @address)";
+            commandText = "INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES(@name, @phoneNumber, @address)";
 
             await _repository.ExecuteAsync(commandText, CommandType.Text, parameters);
         }
 
         public async Task<Person> FindPerson(string firstName, string lastName)
         {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
             SQLiteParameter[] parameters = new SQLiteParameter[]
             {
                 new SQLiteParameter("@name", $"%{firstName} {lastName}%"),
